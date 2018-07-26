ec01ac5a1d0a54ad14a7f4d6bdb0034d6e1662bd
Jorge Cano <jcano@SPARKEMPLOYEE-INSPIRON-5547.local>
2018-07-26 17:54:53 -0500

    react and firebase hosted app and db

---
+++ src/fire.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase'
+var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
+  apiKey: "unreadablestuff",
+  authDomain: "your-domain-name.firebaseapp.com",
+  databaseURL: "https://your-domain-name.firebaseio.com",
+  projectId: "****",
+  storageBucket: "your-domain-name.appspot.com",
+  messagingSenderId: "123123123123"
+};
